e9ca18f2e9d275371231dcfd7248249c6125e856
Shaharyar996 <shaheryar0304@gmail.com>
2018-09-28 11:42:26 +0500

    Initial commit

---
+++ app/src/main/java/com/example/Shaharyar/TaskSmart/ReminderDialogHelper.java
@@ -0,0 +1,5 @@
+package com.example.Shaharyar.TaskSmart;
+
+
+public class ReminderDialogHelper {
+}
---
+++ app/src/main/java/com/example/Shaharyar/TaskSmart/ReminderParams.java
@@ -0,0 +1,13 @@
+package com.example.Shaharyar.TaskSmart;
+
+
+
+public class ReminderParams {
+
+  public static final String ID = "_id";
+  public static final String TYPE = "type";
+  public static final String TITLE = "title";
+  public static final String CONTENT = "content";
+  public static final String TIME = "time";
+  public static final String FREQUENCY = "frequency";
+}
