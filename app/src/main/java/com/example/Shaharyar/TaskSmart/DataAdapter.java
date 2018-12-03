67d1085b0e0092630158e1607173d9eee027be1a
Shaharyar996 <shaheryar0304@gmail.com>
2018-12-03 14:33:22 +0500

    DataAdapter added!

---
+++ app/src/main/java/com/example/Shaharyar/TaskSmart/DataAdapter.java
@@ -0,0 +1,45 @@
+package com.example.Shaharyar.TaskSmart;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DataAdapter extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String urlString = params[0]; // URL to call
+            String data = params[1]; //data to post
+            OutputStream out = null;
+
+
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                out = new BufferedOutputStream(urlConnection.getOutputStream());
+
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+                writer.write(data);
+                writer.flush();
+                writer.close();
+                out.close();
+
+                urlConnection.connect();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            return urlString;
+        }
+
+    }
+
+
